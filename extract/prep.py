d5ac9e9659ad11d6e98b2f0f3d5286f476a139b0
oberljn <oberljn@gmail.com>
2020-06-20 01:14:50 +0000

    In extract/prep.explore getting target=1 over total

--- extract/prep.py
+++ extract/prep.py
@@ -11,9 +11,37 @@ def uscore(df):
         recol = col.replace('.','_')
         df.rename(columns={col:recol}, inplace=True)
 
+def prefilter(df):
+    df = df[df._ws_col_Protocol!='ARP']
+
+
+def label(df):
+    # Label the target
+
+    # Where the infected packet is known by its src or dst IP(s)
+    df['test'] = 'allahu'
+    for ip in conf['infectedip']:
+        df.loc[ (df.ip_src==ip) | (df.ip_dst==ip), 'infected' ]
+
+
+def tcpflag_map():
+    # create funcion that takes flag hashes and transforms into text, eg synack
+    # first get unique list of the hashes
+    # for each on that list, convert to binary - split into list -
+    # which is used like a filter against text list in order 
+    # check https://www.manitonetworks.com/flow-management/2016/10/16/decoding-tcp-flags
+    pass
+
+
+def drop(df):
+    df.drop(columns=['ip_src', 'ip_dst'], inplace=True)
 
 def explore(df):
 
+    # Whats the target makeup
+    #here df.infected.sum() / df.shape[0]
+'''
+    #add change below to protocols by target 1|0
     # Report on transport layer protocols by IP, scaled
     ipproto = df[['ip_src', 'ip_dst']].join(pd.get_dummies(df.ip_proto))
     ipproto.rename(columns={6:'tcp',17:'udp'}, inplace=True) #add mapping file
@@ -42,6 +70,9 @@ def explore(df):
 
     print(protosum.to_string())
 
+'''
+    #add Ports report
+
 
 def select_ports(df):
     with open('utils/ports.yaml') as f:
@@ -52,11 +83,13 @@ def select_ports(df):
 def prep():
     # Main prep function takes dataframe through transformations
 
+    #add Should probably just make a class for df
     csv_path = os.path.join(conf['fspath'], conf['processfile'] + '.csv')
     df = pd.read_csv(csv_path)
-    
     uscore(df)
-
+    prefilter(df)
+    
+    drop(df)
     explore(df)
 
     print(df.head().to_string(index=False))
